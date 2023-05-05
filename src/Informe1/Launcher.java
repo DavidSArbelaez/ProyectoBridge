8ed822ac679231d7a46316c73c24017f4f64f0af
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2023-05-05 16:09:27 -0500

    Update Launcher.java

--- src/Informe1/Launcher.java
+++ src/Informe1/Launcher.java
@@ -3,6 +3,17 @@ package Informe1;
 
 public class Launcher {
     public static void main(String[] args) {
+        
+        Informe informe1 = new Informe();
+        OtroInforme informe2 = new OtroInforme();
+        InformeInterface adapter = new InformeAdapter(informe1, informe2);
+    
+        adapter.PromedioTipo();    
+        adapter.cantidadRespuestas();    
+        adapter.cantidadRespuestasTipo();    
+        adapter.definirPregunta();  
+        adapter.definirPregunta(2);
+        
         Informe inf = new Informe();
         inf.hacerPreguntaAleatoriaTipo(5, 3);
         inf.cantidadRespuestasTipo();
